f2a4f274cecb03b579240f854e590cfb82094c4c
Armine Aroyan <aroanarmine068@gmail.com>
2025-12-26 13:40:39 +0500

    Task0

--- Tyuiu.AroyanAV.Sprint6.Task0.V19.Test/DataServiceTest.cs
+++ Tyuiu.AroyanAV.Sprint6.Task0.V19.Test/DataServiceTest.cs
@@ -4,13 +4,13 @@ namespace Tyuiu.AroyanAV.Sprint6.Task0.V19.Test
 {
     public class DataServiceTest
     {
-            [TestMethod]
-            public void TestMethod1()
-            {
-                DataService ds = new DataService();
-                double res = ds.Calculate(3);
-                double wait = -1;
-                Assert.AreEqual(wait, res);
-            }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = -1;
+            Assert.AreEqual(wait, res);
         }
     }
+}
